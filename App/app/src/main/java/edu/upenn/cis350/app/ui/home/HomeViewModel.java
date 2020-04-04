dcc02e409c38798ebf6de31941dcc0bc2b3510c6
Allison <allison@Allisons-MacBook-Air-3.local>
2020-04-04 17:03:21 -0400

    first commit

---
+++ App/app/src/main/java/edu/upenn/cis350/app/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package edu.upenn.cis350.app.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Swapapp");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
