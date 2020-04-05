c5a8c0b15574f655c818c79c33fc01b7ea74eded
Kaung Khant <kaungkhant@Kaungs-MacBook-Pro.local>
2020-04-05 13:44:32 -0400

    attempts with Volley

--- Final_Project/app/src/main/java/edu/upenn/cis350/final_project/SignupActivity.java
+++ Final_Project/app/src/main/java/edu/upenn/cis350/final_project/SignupActivity.java
@@ -12,32 +12,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-
-import com.android.volley.toolbox.StringRequest;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-
-import java.io.UnsupportedEncodingException;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity {
     static final int IMAGE_PICK_ID = 500;
     static final int REGISTER_ID = 501;
     static final int LOGIN_ACTIVITY_ID = 2;
 
-    SignupActivity activity = this;
     ImageView picView;
     Button profPic;
 
@@ -82,65 +62,17 @@ public class SignupActivity extends AppCompatActivity {
         startActivityForResult(i, IMAGE_PICK_ID);
     }
 
-    public void onSignupButtonClick(View v) throws IOException {
+    public void onSignupButtonClick(View v) {
         boolean valid = validateData();
         if (valid) {
             registerUser();
         }
     }
 
-    void registerUser() throws IOException {
-
-        final RequestQueue queue = MySingleton.getInstance(this.getApplicationContext()).
-                getRequestQueue();
-        final String url = "http://localhost:3000/createNewUser"; // your URL
-        queue.start();
-
-       StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                            Toast.makeText(activity, response, Toast.LENGTH_SHORT).show();
-                           // DisplayText.setText(response.getString("message"));
-                    }
-                }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                parseVolleyError(error);
-                Toast.makeText(activity, "That didn't work!", Toast.LENGTH_SHORT).show();
-                //DisplayText.setText("That didn't work!");
-            }
-        }) {
-           @Override
-           protected Map<String, String> getParams()
-           {
-               Map<String, String>  params = new HashMap<String, String>();
-               params.put("username", email.getText().toString()); // the entered data as the JSON body.
-               params.put("password", password.getText().toString());
-               return params;
-           }
-       };
-
-        MySingleton.getInstance(this).addToRequestQueue(stringRequest);
-/*
+    void registerUser() {
         //for now just returns back to login
         Intent i = new Intent(this, LoginActivity.class);
-        startActivityForResult(i, LOGIN_ACTIVITY_ID);*/
-    }
-
-    public void parseVolleyError(VolleyError error) {
-        try {
-            String responseBody = new String(error.networkResponse.data, "utf-8");
-            JSONObject data = new JSONObject(responseBody);
-            JSONArray errors = data.getJSONArray("errors");
-            JSONObject jsonMessage = errors.getJSONObject(0);
-            String message = jsonMessage.getString("message");
-            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
-
-        } catch (JSONException e) {
-        } catch (UnsupportedEncodingException errorr) {
-        }
+        startActivityForResult(i, LOGIN_ACTIVITY_ID);
     }
 
     boolean validateData() {
