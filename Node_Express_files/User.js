c8f77dd88c76c09f0bb537abffaba689db76a09f
maxnorleans <38734041+maxnorleans@users.noreply.github.com>
2020-04-16 16:30:40 -0400

    login and signup

--- Node_Express_files/User.js
+++ Node_Express_files/User.js
@@ -2,7 +2,9 @@ var mongoose = require("mongoose");
 
 // the host:port must match the location where you are running MongoDB
 // the "myDatabase" part can be anything you like
-mongoose.connect('mongodb://localhost:27017/CIS_350_Final_Project_Database');
+// mongoose.connect('mongodb://localhost:27017/CIS_350_Final_Project_Database');
+mongoose.connect("mongodb+srv://Max:Max@cis350project-8hdrl.mongodb.net/test?retryWrites=true&w=majority");
+// above: running mongoose on Atlas
 
 var Schema = mongoose.Schema;
 
